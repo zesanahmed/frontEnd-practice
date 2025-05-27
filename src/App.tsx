be7beeebf1d47b0cad415dfa3b19b909271baaa5
zesanahmed <zesanahmed593@gmail.com>
2025-05-27 20:40:36 +0600

    router added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Button } from "@/components/ui/button";
+import MainLayout from "./components/layouts/MainLayout";
 
 function App() {
   return (
     <div className="flex flex-col items-center justify-center min-h-svh">
       <Button>Click me</Button>
+      <MainLayout />
     </div>
   );
 }
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,10 +1,24 @@
 import App from "@/App";
+import About from "@/pages/About";
+import Home from "@/pages/Home";
+
 import { createBrowserRouter } from "react-router-dom";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-    children: [],
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+    ],
   },
 ]);
+
+export default router;
