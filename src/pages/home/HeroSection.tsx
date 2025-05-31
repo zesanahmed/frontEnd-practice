eaf2f5e5ede014a0abfaa04e8c476f8b30d4a6a3
zesanahmed <zesanahmed593@gmail.com>
2025-05-31 20:06:39 +0600

    Responsive Hero Section

--- src/pages/home/HeroSection.tsx
+++ src/pages/home/HeroSection.tsx
@@ -4,18 +4,25 @@ import { Button } from "@/components/ui/button";
 
 const HeroSection = () => {
   return (
-    <Container className="h-[calc(100vh-64px)] grid grid-cols-2 place-content-center">
+    <Container className="h-[calc(100vh-64px)] grid grid-cols-1 lg:grid-cols-2 place-content-center">
       <div>
-        <h1 className="text-6xl font-bold">
+        <h1 className="text-4xl lg:text-6xl font-bold">
           <span className="text-light-slate">Trendy Summer</span>
           <br />
-          <span className="text-dart-slate">Kid Collections</span>
+          <span className="text-dark-slate">Kid Collections</span>
         </h1>
-        <p></p>
+        <p className="text-dark-slate max-w-[25ch] mt-10 mb-5 text-lg">
+          Free international shipping on orders over{" "}
+          <span className="font-semibold">$199</span>
+        </p>
         <Button>Book a service</Button>
       </div>
-      <div>
-        <img src={kidCollections} alt="" />
+      <div className="flex items-center justify-center ">
+        <img
+          className="lg:w-[350px] -rotate-[30deg]"
+          src={kidCollections}
+          alt=""
+        />
       </div>
     </Container>
   );
